5eb0dbd4370788991659ad74cb6c8127c647e731
mad-of-gravity <mad.of.gravity@gmail.com>
2023-01-14 19:37:47 +0200

    adding the elements to the body

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,17 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  document.body.addEventListener('click', () => {
+    let article = null;
+
+    //A for loop that initializes the article element and adds it to the body
+    for (let i = 0; i < 5; i++) {
+      article = document.createElement("article");
+      article.classList.add(".message");
+      article.textContent = `Message ${i}`;
+      document.body.appendChild(article);
+     }
+  });
 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
